aa45bffeedd39b2d584a651cf0544bec5b3a137d
koyel <rkoyel04@gmail.com>
2022-08-01 17:00:59 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className='section about-section'>
+    <h1 className='section-title'>about us</h1>
+    <p>Mixing the right amount of alcohol with the right amount of refresher to make it taste delicious is one thing – but making sure it tastes good without losing the zest of the alcohol is definitely something the Einstein’s of bartending cooked up very carefully.
+
+While one may believe that making a drink being an easy task for the bartender, mixology requires genuine chemistry of components to feed out innovative and excellent cocktails. A good Mix is the behind the scenes of a good cocktail.
+
+Mixology is generally accepted as a refined and in-depth study of the art and craft of mixing drinks. Think of it as the study of the chemistry of drinks, and the mixologist as the professional who studies that. A mixologist is just like a chef but for drinks and not for food.
+
+The science of mixology mainly lies behind two major aspects – the blend of flavors and your glassware.
+
+</p>
+   </section>
+  )
+}
+
+export default About
